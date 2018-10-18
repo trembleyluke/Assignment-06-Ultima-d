b7b4edf9fb5ccf6ae2e6c7ea0291e1b592993145
trembleyluke <42677977+trembleyluke@users.noreply.github.com>
2018-10-18 15:17:37 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,28 @@
 public class Main {
-
+public static int worldtime,year,day,hours,minutes,trammel,felucca;
     public static void main(String[] args) {
-	int worldtime = 67294091;
-	int year = worldtime / 1440 /365 + 1;
-	int day = worldtime/1440 % 365 + 1;
-	int hours = (worldtime/60)%24;
-	int minutes = worldtime % 60;
+		worldtime = 67294091;
+		time();
+		Trammel();
+		Felucca();
+	}
+	public static void time() {
+			year = worldtime / 1440 / 365 + 1;
+			day = worldtime / 1440 % 365 + 1;
+			hours = (worldtime / 60) % 24;
+			minutes = worldtime % 60;
 
-	System.out.printf("It is %d:%02d on day %d of the year %2d\n",hours, minutes, day, year);
+			System.out.printf("It is %d:%02d on day %d of the year %2d\n", hours, minutes, day, year);
+		}
 
-	int trammel = worldtime / 1440 % 9 +1;
-	int Felucca = worldtime / 1440 %14 +1;
-
-	System.out.printf("Trammel is in day %d of its 9 day cycle\n", trammel);
-	System.out.printf("Felucca is in day %d of its 14 day cycle\n", Felucca);
-
+		public static void Trammel() {
+			trammel = worldtime / 1440 % 9 + 1;
+			System.out.printf("Trammel is in day %d of its 9 day cycle\n", trammel);
+		}
+		public static void Felucca() {
+			felucca = worldtime / 1440 % 14 + 1;
+			System.out.printf("Felucca is in day %d of its 14 day cycle\n", felucca);
+		}
 
     }
-}
+
